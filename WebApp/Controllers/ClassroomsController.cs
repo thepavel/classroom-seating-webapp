00182637a532a99f8924e71ff2dd2f0c31aafece
pavel <lutskovp@gmail.com>
2023-11-21 23:59:49 -0800

    add basic api controller

---
+++ WebApp/Controllers/ClassroomsController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[Route("api/[controller]")]
+public class ClassroomsController : Controller
+{
+    public ClassroomsController()
+    { }
+
+    [HttpGet]
+    public IEnumerable<string> GetClassrooms()
+    {
+        return new List<string>() {"a", "new", "string", "list"};
+    }
+
+
+
+}
+
