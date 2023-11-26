5d35c1581bf74974eeab5ab000c7cb6e518cbd62
pavel <lutskovp@gmail.com>
2023-11-26 00:08:57 -0800

    loads class rosters for defined periods

--- WebApp/Services/ClassroomInfoService.cs
+++ WebApp/Services/ClassroomInfoService.cs
@@ -6,15 +6,25 @@ namespace WebApp.Services
     public class ClassroomInfoService
     {
         private readonly string _dataFile = "classroom.json";
-        private readonly string _classroomRostersFile = "data\\classes.json";
+        private readonly string _classroomRostersFile = "data/classes.json";
 
         private readonly ClassroomModel classroom;
+        private readonly List<PeriodRoster> periodRosters;
 
         public ClassroomInfoService()
         {
             
             classroom = LoadClassroomInfo();
+            periodRosters = LoadPeriodRosters();
+        }
 
+        private List<PeriodRoster> LoadPeriodRosters()
+        {
+            using StreamReader r = new(_classroomRostersFile);
+            string json = r.ReadToEnd();
+
+            var periodRosters = JsonSerializer.Deserialize<PeriodRoster[]>(json) ?? Array.Empty<PeriodRoster>();
+            return new List<PeriodRoster>(periodRosters);
         }
 
         private ClassroomModel LoadClassroomInfo()
