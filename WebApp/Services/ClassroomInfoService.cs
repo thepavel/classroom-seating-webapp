e66a9b5d21ef0adc563c4589559df4e1a280750c
pavel <lutskovp@gmail.com>
2023-11-26 00:01:10 -0800

    adds basic class roster config

--- WebApp/Services/ClassroomInfoService.cs
+++ WebApp/Services/ClassroomInfoService.cs
@@ -6,15 +6,24 @@ namespace WebApp.Services
     public class ClassroomInfoService
     {
         private readonly string _dataFile = "classroom.json";
+        private readonly string _classroomRostersFile = "data\\classes.json";
 
         private readonly ClassroomModel classroom;
 
         public ClassroomInfoService()
         {
+            
+            classroom = LoadClassroomInfo();
+
+        }
+
+        private ClassroomModel LoadClassroomInfo()
+        {
             using StreamReader r = new(_dataFile);
             string json = r.ReadToEnd();
-            classroom = JsonSerializer.Deserialize<ClassroomModel>(json) ?? new ClassroomModel();
-
+            var classroom = JsonSerializer.Deserialize<ClassroomModel>(json) ?? new ClassroomModel();
+            r.Close();
+            return classroom;
         }
 
         public ClassroomModel GetClassroomInfo()
