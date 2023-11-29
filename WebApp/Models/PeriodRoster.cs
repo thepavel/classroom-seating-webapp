9c2716306fbfff322c66d46e83ed97684330ba9b
pavel <lutskovp@gmail.com>
2023-11-28 22:54:23 -0800

    show sorted roster on page

--- WebApp/Models/PeriodRoster.cs
+++ WebApp/Models/PeriodRoster.cs
@@ -2,7 +2,16 @@ namespace WebApp.Models
 {
     public class PeriodRoster
     {
+        public PeriodRoster()
+        {
+            StudentNames = Array.Empty<string>();
+        }
         public int Period { get; set; }
-        public required string[] StudentNames { get; set; }
+        public string[] StudentNames { get; set; }
+        public string[] GetSortedRoster()
+        {
+            Array.Sort(StudentNames);
+            return StudentNames;
+        }
     }
 }
