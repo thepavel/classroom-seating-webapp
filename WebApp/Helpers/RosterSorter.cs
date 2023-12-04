85e37c13a2993ddf8cb8c02f83aa6aec96df247c
pavel <lutskovp@gmail.com>
2023-12-03 23:31:01 -0800

    adds fullname property

--- WebApp/Helpers/RosterSorter.cs
+++ WebApp/Helpers/RosterSorter.cs
@@ -12,21 +12,5 @@ namespace WebApp.Helpers
                         select studentName).ToArray();
         }
 
-        public static string[] GetSortedStudents(string[] names)
-        {
-            return (from name in names
-                    let parsedName = name.Split(" ")
-                    let studentName = new StudentName(parsedName.First(), parsedName.Last())
-                    orderby studentName.LastName descending
-                    select $"{studentName.FirstName} {studentName.LastName}"
-                    ).ToArray();
-
-        }
-
-        public static string[] SortStudentNames(PeriodRoster roster)
-        {
-            
-            return roster.GetSortedRoster();
-        }
     }
 }
