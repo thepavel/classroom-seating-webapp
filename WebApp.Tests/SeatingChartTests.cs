5ac7401298303c75f5a0f0bb898f674a59b3396c
pavel <lutskovp@gmail.com>
2023-12-06 22:46:25 -0800

    returns rows*columns sized 2d array filled with x

--- WebApp.Tests/SeatingChartTests.cs
+++ WebApp.Tests/SeatingChartTests.cs
@@ -5,6 +5,8 @@ namespace WebApp.Tests
 {
     public class SeatingChartTests
     {
+        private const int DefaultRows = 4;
+        private const int DefaultColumns = 4;
 
         [Fact]
         public void SeatingChart_Constructor_Chart_IsNotNull()
@@ -16,11 +18,31 @@ namespace WebApp.Tests
         [Fact]
         public void SeatingChart_Constructor_ChartDimensions_AreRowsTimesColumns()
         {
-            var seatingChart = new SeatingChart(4, 4);
-            seatingChart.Chart[0].Capacity.ShouldBe(4);
-            
+            int rows = DefaultRows;
+            int columns = DefaultColumns;
+            var seatingChart = new SeatingChart(rows, columns);
+
+            seatingChart.Chart.Length.ShouldBe(rows * columns);            
         }
 
+        [Fact]
+        public void SeatingChart_GetStudentSeatingChart_Returns2DArray_WithX() 
+        {
+            int rows = DefaultRows;
+            int columns = DefaultColumns;
+            var seatingChart = new SeatingChart(rows, columns);
+            var studentSeatingChart = seatingChart.GetStudentSeatingChart();
+            
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    studentSeatingChart[i, j].ShouldBe("x");
+                }
+            }
+            
+            
+        }
         // [Fact]
         // public void SeatingChart_EmptyChart_PutsFirstStudentInTopRight()
         // {
--- WebApp/Helpers/SeatingChart.cs
+++ WebApp/Helpers/SeatingChart.cs
@@ -5,19 +5,44 @@ namespace WebApp.Helpers
         public int Rows { get; private set; }
         public int Columns { get; private set; }
 
-        public List<StudentName>[] Chart { get; private set; }
+        public string[,] Chart { get; private set; }
+        public List<StudentName> Students { get; private set; }
+
+        //public StudentName[,] StudentSeatingChart { get; private set; }
+
+        public string[,] GetStudentSeatingChart()
+        {
+            // var row = Enumerable.Repeat("x", Columns).ToArray();
+            return CreateDefaultSeatingChart();
+        }
+
+        private string[,] CreateDefaultSeatingChart()
+        {
+
+            const string defaultValue = "x";
+            var chart = new string[Columns, Rows];
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    chart[i, j] = defaultValue;
+                }
+            }
+
+            return chart;
+        }
 
         public SeatingChart(int rows, int columns)
         {
             Rows = rows;
             Columns = columns;
-            Chart = new List<StudentName>[columns];
 
-            for (var i = 0; i < rows; i++) {
-                Chart[i] = new List<StudentName>(capacity: rows);
-            }
-            
+            //fill with x's initially. seating chart takes StudentNames and outputs strings. start with 'x'
+            Chart = GetStudentSeatingChart();
+            Students = new List<StudentName>();
         }
+
     }
 
 
