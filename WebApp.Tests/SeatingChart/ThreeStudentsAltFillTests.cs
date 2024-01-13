f7aa91f9dc4e4c3180f0a0b09c308b51c2f7e7c7
pavel <lutskovp@gmail.com>
2024-01-13 00:53:47 -0800

    add three student tests

---
+++ WebApp.Tests/SeatingChart/ThreeStudentsAltFillTests.cs
@@ -0,0 +1,31 @@
+﻿using Shouldly;
+using WebApp.Helpers;
+
+namespace WebApp.Tests;
+
+public class ThreeStudentsAltFillTests
+{
+    public ThreeStudentsAltFillTests()
+    {
+        Students = new List<StudentName> {
+            new("first", "student"),
+            new("next", "pupil"),
+            new("third", "one")
+        };
+    }
+
+    public List<StudentName> Students { get; }
+
+    [Fact]
+    public void ThirdStudentIsPlacedIn_CorrectPlace()
+    {
+        //given
+        var seatingChart = new SeatingChart(2, 3, Students, true);
+
+        //when
+        var thirdStudent = Students[2].FullName;
+
+        //then
+        seatingChart.Chart[1,1].ShouldBe(thirdStudent);
+    }
+}
