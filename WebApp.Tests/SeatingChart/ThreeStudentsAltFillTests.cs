7644aac18c91dbab1c82d4b8ca3acc91eb7764da
pavel <lutskovp@gmail.com>
2024-01-18 22:07:54 -0800

    adds 2x2 test

--- WebApp.Tests/SeatingChart/ThreeStudentsAltFillTests.cs
+++ WebApp.Tests/SeatingChart/ThreeStudentsAltFillTests.cs
@@ -17,6 +17,8 @@ public class ThreeStudentsAltFillTests
     public static IEnumerable<object[]> SecondStudent_TestData => new List<object[]> {
         new object[] { 3, 1, 1, 0 }, // second student should be in 1st spot in 2nd row [1,0]
         new object[] { 1, 3, 0, 1 }, // second student should be in middle spot in 1st row [0,1]
+        new object[] { 2, 2, 0, 1 }, // second student should be in 1st row [0,1]
+
     };
 
     public List<StudentName> Students { get; }
@@ -36,6 +38,7 @@ public class ThreeStudentsAltFillTests
     }
 
     [Theory]
+    [InlineData(2, 2, 1, 0)] //a 2x2 classroom should put 3rd student in 2nd row [1,0]
     [InlineData(1, 3, 0, 2)] //a 1x3 classroom should put 3rd student in 3rd spot in 1st row [0,2]
     [InlineData(3, 1, 2, 0)] //a 3x1 classroom should put 3rd student into 3rd row [2,0]
     [InlineData(2, 3, 1, 1)] //a 2x3 classroom should put 3rd student in the back row between the first two students [1,1]
