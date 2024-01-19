c339615f2a698450c1b61efcdd0eaf5fa90d2365
pavel <lutskovp@gmail.com>
2024-01-18 22:26:19 -0800

    adds 3-student 1x4 and 4x1 tests

--- WebApp.Tests/SeatingChart/ThreeStudentsAltFillTests.cs
+++ WebApp.Tests/SeatingChart/ThreeStudentsAltFillTests.cs
@@ -16,9 +16,13 @@ public class ThreeStudentsAltFillTests
 
     public static IEnumerable<object[]> SecondStudent_TestData => new List<object[]> {
         new object[] { 3, 1, 1, 0 }, // second student should be in 1st spot in 2nd row [1,0]
+        new object[] { 4, 1, 1, 0 }, // 4x1: second student should be in 2nd row [1,0]
+        
         new object[] { 1, 3, 0, 1 }, // second student should be in middle spot in 1st row [0,1]
+        new object[] { 1, 4, 0, 1 }, // second student should be next to 1st student in 1x4 [0,1]
+        
         new object[] { 2, 2, 0, 1 }, // second student should be in 1st row [0,1]
-
+        
     };
 
     public List<StudentName> Students { get; }
@@ -40,7 +44,9 @@ public class ThreeStudentsAltFillTests
     [Theory]
     [InlineData(2, 2, 1, 0)] //a 2x2 classroom should put 3rd student in 2nd row [1,0]
     [InlineData(1, 3, 0, 2)] //a 1x3 classroom should put 3rd student in 3rd spot in 1st row [0,2]
+    [InlineData(1, 4, 0, 3)] //a 1x4 classroom should put 3rd student in last spot [0,3]
     [InlineData(3, 1, 2, 0)] //a 3x1 classroom should put 3rd student into 3rd row [2,0]
+    [InlineData(4, 1, 3, 0)] //a 4x1 classroom should put 3rd student into last row [3,0]
     [InlineData(2, 3, 1, 1)] //a 2x3 classroom should put 3rd student in the back row between the first two students [1,1]
     [InlineData(3, 2, 2, 0)] //a 3x2 classroom should put 3rd student in the back row [2,0]
     [InlineData(2, 4, 1, 1)] //a 2x4 classroom should put 3rd student in 2nd row [1,1]
